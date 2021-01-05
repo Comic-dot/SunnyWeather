b34acc692cf2ae7e441154d21d7169f13b0aeb20
Comic-dot <2216960672@qq.com>
2021-01-05 14:29:45 +0800

    update

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.java
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.java
@@ -1,6 +1,15 @@
 package com.sunnyweather.android;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
+import android.content.Context;
 
 public class SunnyWeatherApplication extends Application {
+    public static  final String TOKEN = "OSFdbLzFF72j4Rko";
+    @SuppressLint("StaticFieldLeak")
+    static public Context context;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context =getApplicationContext(); }
 }
